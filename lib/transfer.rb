48a4ed0f2e81cc4f46650c074f3231226debdf4f
Anuj Panchal <anujpan1@gmail.com>
2018-10-30 03:29:51 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -21,7 +21,7 @@ class Transfer
       @sender.balance -= @amount
       @receiver.balance += @amount
       @status = "complete"
-    else
+    elsif !@sender.valid?
       "Transaction rejected. Please check your account balance."
     end
     # binding.pry
