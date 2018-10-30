c8de758db09806c8ba059b7877c4cbe85bba17b3
Anuj Panchal <anujpan1@gmail.com>
2018-10-30 03:37:29 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -27,7 +27,7 @@ class Transfer
   end
   
   def reverse_transfer
-    if @status == "complete" && @receiver.valid? && @receiver.balance > @amount
+    if @status == "complete"
       @receiver.balance -= @amount
       @sender.balance += @amount
       @status = "reversed"
