a180420401989807c88b6e87b394ee38813e2d4c
Anuj Panchal <anujpan1@gmail.com>
2018-10-30 03:36:23 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -27,13 +27,8 @@ class Transfer
   end
   
   def reverse_transfer
-    if @status == "pending" && @sender.valid? && @sender.balance > @amount
-      @receiver.balance -= @amount
-      @sender.balance += @amount
-      @status = "complete"
-    else
-      @status = "rejected"
-      "Transaction rejected. Please check your account balance."
-    end
+    @receiver.balance -= @amount
+    @sender.balance += @amount
+    @status = "complete"
   end
 end
